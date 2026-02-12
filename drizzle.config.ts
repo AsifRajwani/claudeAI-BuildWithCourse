831075ccef5c891aa278df313de00b923abec77f
AsifRajwani <asif_rajwani@yahoo.com>
2026-02-12 10:49:28 -0500

    Section2 working

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from "drizzle-kit";
-import * as dotenv from "dotenv";
-
-dotenv.config({ path: ".env.local" });
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: "./src/db/migrations",
-  schema: "./src/db/schema.ts",
-  dialect: "postgresql",
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
